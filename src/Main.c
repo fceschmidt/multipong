e452d3faa022f6cf513405c2261d95f09d467e90
Fabian Schmidt <fschmidt95@googlemail.com>
2016-01-08 17:42:52 +0100

    Add main function.

---
+++ src/Main.c
@@ -0,0 +1,12 @@
+/*
+====================
+main
+
+Entry point of the application. Starts the program.
+====================
+*/
+int main( int argc, char *argv[] ) {
+	// TODO: Implement this function.
+
+	return 0;
+}
