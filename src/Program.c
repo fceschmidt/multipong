491332c1b33124d7de2f2202d3e3c49c28e143c1
Fabian Schmidt <fschmidt95@googlemail.com>
2016-01-08 18:40:46 +0100

    Add code which handles command line arguments.

--- src/Program.c
+++ src/Program.c
@@ -1,8 +1,39 @@
 #include "Program.h"
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
 
 static void DestroyResources( void );
+static int ReadArguments( int argc, char *argv[] );
+
+static int ArgumentHelp( void );
+
+/*
+==========================================================
+
+A function that is used to set a command line option.
+
+==========================================================
+*/
+typedef int( *setArgumentFunction_t )( void );
+
+/*
+==========================================================
+
+A couple of a name and the implementation function of a command line option.
+
+==========================================================
+*/
+struct ArgumentNameFunctionCouple {
+	const char *			name;
+	setArgumentFunction_t 	function;
+};
+
+// The map of names and functions for the command line options parser.
+// TODO: Add complete list of options HERE.
+static struct ArgumentNameFunctionCouple argumentNameFunctionMap[] = {
+	{ .name = "--help", .function = &ArgumentHelp }
+};
 
 /*
 ====================
@@ -16,6 +47,7 @@ int InitializeProgram( int argc, char *argv[] ) {
 	atexit( DestroyResources );
 
 	// TODO: Read command line arguments.
+	ReadArguments( argc, argv );
 	
 	// TODO: Remove this hello message and print it in debug.
 	printf( "Successfully started multipong.\n" );
@@ -42,4 +74,51 @@ Destroys all resources used by the program.
 */
 static void DestroyResources( void ) {
 	// TODO: Call all destructors.
+}
+
+/*
+====================
+ReadArguments
+
+Reads the arguments from the command line.
+	--help		prints a help message
+	--windowed	TODO executes in windowed mode
+====================
+*/
+static int ReadArguments( int argc, char *argv[] ) {
+	int i;
+	int numImplementedArgs = sizeof( argumentNameFunctionMap ) / sizeof( struct ArgumentNameFunctionCouple );
+	int j;
+	int result;
+
+	// Go through the arguments
+	for( i = 0; i < argc; i++ ) {
+		// Go through the implemented arguments map
+		for( j = 0; j < numImplementedArgs; j++ ) {
+			// If any entry's name matches the option:
+			if( !strcmp( argumentNameFunctionMap[j].name, argv[i] ) ) {
+				// Call the function from the map entry.
+				result = argumentNameFunctionMap[j].function();
+				// If it failed, return the failure.
+				if( result ) {
+					return result;
+				}
+			}
+		}
+	}
+
+	return 0;
+}
+
+/*
+====================
+ArgumentHelp
+
+Implements behaviour when the program is started with --help.
+Prints a help message.
+====================
+*/
+int ArgumentHelp( void ) {
+	printf( "Welcome to multipong. There is no help available at the moment, sorry!\n" );
+	return 0;
 }
