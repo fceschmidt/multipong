eb9a7b9a8990bb54c1de5da68e8a327e5795729f
Fabian Schmidt <fschmidt95@googlemail.com>
2016-01-12 15:47:56 +0100

    Comment out assertion printing when the assertion is met.

--- src/Debug/Debug.h
+++ src/Debug/Debug.h
@@ -8,8 +8,8 @@ int CloseDebug ();
 
 #define DebugAssert( x ) if ( !( x ) ) { \
 	DebugPrintF( "----[FAIL]----: %s\n\t@%s (%s:%d)", #x, __func__, __FILE__, __LINE__ );\
-} else { \
+}/* else { \
 	DebugPrintF( "++++[PASS]++++: %s", #x );\
-}
+}*/
 
 #endif // DEBUG_H_INCLUDED
