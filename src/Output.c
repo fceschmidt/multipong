c0ac99c82fbc124c9d1ff138b56ad0d68155f5c7
Fin Töter <f.toeter@googlemail.com>
2016-01-18 16:49:16 +0000

    Updated Output

--- src/Output.c
+++ src/Output.c
@@ -1,12 +1,18 @@
 #include <SDL2/SDL.h>
+#include <SDL2/SDL_image.h>
 #include "Debug/Debug.h"
 #include "Output.h"
 #include "Physics.h"
+#include "Game.h"
 
 // Variables
 static SDL_Window *		sdlWindow = NULL;
 static SDL_Renderer *	sdlRenderer = NULL;
 int						outputFullscreen = 0;
+static SDL_Texture*     paddleTexture ;
+static SDL_Texture*     ballTexture ;
+static SDL_Texture*     backgroundTexture;
+static SDL_Surface*     temp ;
 
 // Functions
 SDL_Window *GetSdlWindow( void );
@@ -20,14 +26,28 @@ Creates the window and renderer.
 ====================
 */
 int InitializeGraphics( void ) {
+    outputFullscreen = 1 ;
 	DebugAssert( !SDL_Init( SDL_INIT_VIDEO ) );
-	sdlWindow = SDL_CreateWindow( "multipong", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 640, 480, outputFullscreen ? SDL_WINDOW_FULLSCREEN_DESKTOP : SDL_WINDOW_SHOWN );
+	sdlWindow = SDL_CreateWindow( "multipong", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 800, 600, outputFullscreen ? SDL_WINDOW_FULLSCREEN_DESKTOP : SDL_WINDOW_SHOWN );
 	DebugAssert( sdlWindow );
 	sdlRenderer = SDL_CreateRenderer( sdlWindow, -1, SDL_RENDERER_ACCELERATED );
 	DebugAssert( sdlRenderer );
-	DebugAssert( SDL_SetHint( SDL_HINT_RENDER_SCALE_QUALITY, "1" ) );
+	temp = IMG_Load("Assets/Ball.png" );
+    ballTexture = SDL_CreateTextureFromSurface( sdlRenderer, temp );
+    //temp = IMG_Load("Assets/Paddle.png" );
+    //paddleTexture = SDL_CreateTextureFromSurface( renderer, temp );
+
+	//DebugAssert( SDL_SetHint( SDL_HINT_RENDER_SCALE_QUALITY, "1" ) );
 	atexit( SDL_Quit );
 	return !( sdlWindow && sdlRenderer );
+}
+
+
+void SetWindowResolution (int  Width , int Height , int Fullscreen) {
+    SDL_DestroyWindow(sdlWindow);
+    SDL_DestroyRenderer(sdlRenderer);
+    sdlWindow = SDL_CreateWindow( "multipong", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, Width, Height, Fullscreen ? SDL_WINDOW_FULLSCREEN_DESKTOP : SDL_WINDOW_SHOWN );
+    sdlRenderer = SDL_CreateRenderer( sdlWindow, -1, SDL_RENDERER_ACCELERATED );
 }
 
 /*
@@ -73,7 +93,7 @@ int DisplayGameState( const struct GameState *state ) {
 	 *		This is an internal definition, SDL does not do this for you.
 	 * 		The coordinates on this square go from (-1,1) in the top left corner
 	 *		to (1,-1) in the bottom right corner. You need these because in the
-	 *		GameState, everything is saved on a coordinate system from -1 to 1 on 
+	 *		GameState, everything is saved on a coordinate system from -1 to 1 on
 	 *		the x and y axes.
 	 * 3. Clear the renderer.
 	 * 4. (Optional) Draw the background.
@@ -96,6 +116,39 @@ int DisplayGameState( const struct GameState *state ) {
 	 * 7. Present and be done!
 	 * P.S.: You can test this function for ten seconds by running the program, going into Host game mode and pressing enter in the lobby!
 	*/
+
+    int            windowWidth, windowHeight,i ;
+    SDL_Rect       ball_rect;
+    SDL_GetWindowSize( sdlWindow, &windowHeight, &windowHeight );
+    struct Point2D        paddleStart ;
+    struct Point2D        paddleEnd ;
+    struct Point2D        pointBall ;
+    int            radius    ;
+
+    //Set all corresponding Rect Values
+    //calculateBallCoordinates(state->ball,&pointBall,&radius) ;
+    ball_rect.w = radius*2 ;
+    ball_rect.h = radius*2 ;
+    ball_rect.x = (pointBall.x - radius) ;
+    ball_rect.y = (pointBall.y - radius) ;
+
+
+
+    //copy everything in the renderer
+	SDL_RenderClear( sdlRenderer );
+
+
+
+
+	for (i = 0 ; i < state->numPlayers ; i++){
+        SDL_SetRenderDrawColor( sdlRenderer, 0x00, 0x00, 0xFF, 0xFF );
+       // calculatePaddleCoordinates (state,&paddleStart,&paddleEnd);
+        SDL_RenderDrawLine( sdlRenderer, paddleStart.x, paddleStart.y, paddleEnd.x, paddleEnd.y );
+	}
+
+	SDL_RenderCopy( sdlRenderer, ballTexture, NULL, &ball_rect );
+	SDL_RenderPresent( sdlRenderer );
+
 	return 0;
 }
 
